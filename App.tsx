d81e98e7df7cff4172fb18d4ad2b8ebe7a12ae18
dhfai <nerodayo37@gmail.com>
2024-06-22 22:21:27 +0800

    import/export components

---
+++ App.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import { ButtonCustom } from './Components/Buttons/Button';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      
+      <View style= {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{
+          fontSize: 28,
+          fontWeight: 'bold',
+          letterSpacing: 4 }}
+        >
+          HELLO GAMERS!
+        </Text>
+        <Text style={{
+          fontSize: 16, 
+          color: '#222831',
+          textAlign: 'center',
+          margin: 10,
+        }}
+        >
+          welcome to the official site of the "mind maze" game, you must have a game account first to be able to explore more fully on this site.
+        </Text>
+      </View>
+
+      <View style= {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Image
+          source={require('./assets/splash-img.png')}
+          style={{ width: 250, height: 300, marginTop: 50 }}
+        />
+      </View>
+
+        <View style={{ 
+            flex: 1,
+            flexDirection: 'row', 
+            alignItems: 'center' ,
+            marginRight: 30,
+            marginLeft: 30,
+        }}>
+          <ButtonCustom color='#fff' text='Login' backgroundColor='#222831' />
+          <ButtonCustom color='#222831' text='Register' />
+        </View>
+    </View>
+  );
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# LAB PEMROGRAMAN KOMPUTASI BERGEAK
+
+## Deskripsi
+Repository ini berisi file-file yang digunakan dalam mata kuliah Pemrograman Komputasi Bergerak. File-file tersebut berupa file-file latihan, tugas, dan proyek yang dikerjakan selama perkuliahan.
+
+## Setup Environment
+1. Intall Node.js dan NPM
+2. Install Expo CLI
+3. Install Android Studio (optional)
+4. Install Visual Studio Code (optional)
