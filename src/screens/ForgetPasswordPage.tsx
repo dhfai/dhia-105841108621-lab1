c5335d20a50cbdadad9c16b59a713c52f1acfb40
dhfai <nerodayo37@gmail.com>
2024-07-24 04:32:27 +0800

    Navigation and Taskbar Tugas

--- src/screens/ForgetPasswordPage.tsx
+++ src/screens/ForgetPasswordPage.tsx
@@ -17,30 +17,30 @@ const ForgotPasswordPage: React.FC = () => {
 
     return (
         <View style={{ flex: 1, padding: 20, marginTop: 50, }}>
-        <Text style={{ fontSize: 34, marginBottom: 150, textAlign: 'left', fontFamily: 'MetroBold' }}>Forgot password</Text>
-        <Text style={{ textAlign: 'left', marginBottom: 20, fontFamily: 'MetroMedium', fontSize: 16 }}>
-            Please, enter your email address. You will receive a link to create a new password via email.
-        </Text>
-        <TextInputField
-            placeholder="Email"
-            value={email}
-            onChangeText={setEmail}
-            style={error ? { borderColor: 'red' } : {}}
-        />
-        {error ? <Text style={{ color: 'red', textAlign: 'center', marginBottom: 10 }}>{error}</Text> : null}
-        <TouchableOpacity
-            style={{
-            backgroundColor: 'red',
-            paddingVertical: 15,
-            borderRadius: 30,
-            marginVertical: 10,
-            alignItems: 'center',
-            elevation: 3
-            }}
-            onPress={handleSend}
-        >
-            <Text style={{ color: 'white', fontWeight: 'bold' }}>SEND</Text>
-        </TouchableOpacity>
+            <Text style={{ fontSize: 34, marginBottom: 150, textAlign: 'left', fontFamily: 'MetroBold' }}>Forgot password</Text>
+            <Text style={{ textAlign: 'left', marginBottom: 20, fontFamily: 'MetroMedium', fontSize: 16 }}>
+                Please, enter your email address. You will receive a link to create a new password via email.
+            </Text>
+            <TextInputField
+                placeholder="Email"
+                value={email}
+                onChangeText={setEmail}
+                style={error ? { borderColor: 'red' } : {}}
+            />
+            {error ? <Text style={{ color: 'red', textAlign: 'center', marginBottom: 10 }}>{error}</Text> : null}
+            <TouchableOpacity
+                style={{
+                    backgroundColor: 'red',
+                    paddingVertical: 15,
+                    borderRadius: 30,
+                    marginVertical: 10,
+                    alignItems: 'center',
+                    elevation: 3
+                }}
+                onPress={handleSend}
+            >
+                <Text style={{ color: 'white', fontWeight: 'bold' }}>SEND</Text>
+            </TouchableOpacity>
         </View>
     );
 };
